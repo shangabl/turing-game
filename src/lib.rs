1a0c5576d589e690e3e06edad9792d3f048e3ea4
Shane Gable <shangabl@amazon.com>
2024-07-07 19:24:54 -0700

    Build out the Basics including logic gates and combinational circuits

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,10 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct FullAddrOutput {
-    sum: bool,
-    carry: bool,
-}
-
-#[wasm_bindgen]
-impl FullAddrOutput {
-   #[wasm_bindgen(constructor)]
-   pub fn new(sum: bool, carry: bool) -> FullAddrOutput {
-      FullAddrOutput { sum, carry }
-   }
-
-   pub fn get_sum(&self) -> bool {
-    self.sum
-   }
-
-   pub fn get_carry(&self) -> bool {
-    self.carry
-   }
-}
-
-
-// Function to toggle a boolean value (used in Level 1)
-#[wasm_bindgen]
 pub fn toggle_transistor(on: bool) -> bool {
     !on
 }
 
-// Function to perform an AND operation on two boolean values (used in Level 2)
 #[wasm_bindgen]
 pub fn and_gate(input1: bool, input2: bool) -> bool {
     input1 && input2
@@ -75,11 +50,19 @@ pub fn xnor_gate(input1: bool, input2: bool) -> bool {
 }
 
 #[wasm_bindgen]
-pub fn full_adder(a: bool, b: bool, carry_in: bool) -> FullAddrOutput {
-    let sum1 = xor_gate(a, b);
-    let carry1 = and_gate(a, b);
-    let sum2 = xor_gate(sum1, carry_in);
-    let carry2 = and_gate(sum1, carry_in);
-    let carry_out = or_gate(carry1, carry2);
-    FullAddrOutput { sum: sum2, carry: carry_out }
+pub fn set_bit(position: u8) -> u8 {
+    1 << position
+}
+
+#[wasm_bindgen]
+pub struct FullAdderResult {
+    pub sum: bool,
+    pub carry: bool,
+}
+
+#[wasm_bindgen]
+pub fn full_adder(a: bool, b: bool, carry_in: bool) -> FullAdderResult {
+    let sum = a ^ b ^ carry_in; // Sum bit
+    let carry = (a & b) | (carry_in & (a ^ b));
+    FullAdderResult { sum, carry }
 }
